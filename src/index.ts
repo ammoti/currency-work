0939edda77cefe94fcfbf00cf1386161baaeb860
Vahap Yigit <vahap.yigit@gmail.com>
2023-06-12 06:03:34 +0300

    try-json response

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ export default {
 	async fetch(
 		request: Request,
 		ctx: ExecutionContext
-	): Promise<Response> {
+	): Promise<any> {
 		const response = await Parser.parser(request).then(data => {
 			if (data && data !== undefined) {
-				return new Response(JSON.stringify(data));
+				return new Response(JSON.stringify(data)).json();
 			}
 			throw new Error("Currency not found");
 		}).catch(err => {
